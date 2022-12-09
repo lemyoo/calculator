8cced39a53e76189ad5d540bc4d506678e7a4941
@lemyo <emmanuel@EMMANUELLARTEY.localdomain>
2022-12-09 11:33:07 +0000

    can display and delete clear numbers

--- src/components/FunctionButton.js
+++ src/components/FunctionButton.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles'
+import { styled } from '@mui/material/styles';
+import { useSelector, useDispatch } from 'react-redux';
+import { displayNumberAction } from '../Actions/AppActions'
 
 const NumButton = styled(Button)(({ theme }) => ({
     borderRadius: '5px 5px 20px 20px',
@@ -9,8 +11,24 @@ const NumButton = styled(Button)(({ theme }) => ({
 }));
 
 const FunctionButton = ({ number }) => {
+    const dispatch = useDispatch();
+    const { displayValues } = useSelector(state => state.app);
+    const onClickAction = () => {
+        const values = displayValues;
+        switch (number) {
+            case "DEL":
+                values.pop();
+                dispatch(displayNumberAction(values))
+                break;
+            case "AC":
+                dispatch(displayNumberAction([]))
+                break;
+            default:
+                break;
+        }
+    }
     return (
-        <NumButton variant='contained'>{number}</NumButton>
+        <NumButton variant='contained' onClick={onClickAction}>{number}</NumButton>
     )
 }
 
